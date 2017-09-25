3575546b567799eacd00d24ccb2fd5e1b988c4c5
Sergio del Rio <oftheriver@gmail.com>
2017-09-25 18:22:29 +0200

    Fix typo on entrypoint comment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ if [ -z "$CLAYMOREPORT" ]; then
     CLAYMOREPORT=3333
 fi
 
-# Entrypoint that connects to IP $IP:$CLAYMOREPORT, each $FREQUENCY seconds and listn on port $LISTENPORT
+# Entrypoint that connects to IP $IP:$CLAYMOREPORT, each $FREQUENCY seconds and listen on port $LISTENPORT
+
 python /usr/local/bin/claymore-exporter.py -t $IP -f $FREQUENCY -p $LISTENPORT -c $CLAYMOREPORT
 
